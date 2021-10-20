19fcbe83f8aec69e1419e62f3dfb52422203a7da
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2021-10-20 22:59:12 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,7 +5,28 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    // Average
+    sum = 0;
+    for( int i = 0; i < setlength; i++ ){
+        sum += numberset[i];
+    }
+    s.average = sum /setlength;
+    
+    //Max
+    
+    
+    //Min
+    
+}
+    
 }
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
+
+void main()
+{
+   
+    
+}
