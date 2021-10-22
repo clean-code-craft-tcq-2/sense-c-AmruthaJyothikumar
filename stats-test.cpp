58207cf5c4f77232c080bd05872814a76f259e1d
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2021-10-22 10:55:31 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -17,8 +17,7 @@ TEST_CASE("reports average, minimum and maximum") {
 }
 
 TEST_CASE("average is NaN for empty array") {
-    Stats computedStats = compute_statistics(0, 0);
-    
+    Stats computedStats = compute_statistics(0, 0);   
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
@@ -41,9 +40,18 @@ TEST_CASE("raises alerts when max is greater than threshold") {
 
     const float maxThreshold = 10.2;
     check_and_alert(maxThreshold, alerters, computedStats);
-
-    // need a way to check if both emailAlerter, ledAlerter were called
-    // you can define call-counters along with the functions, as shown below
+    REQUIRE(emailAlertCallCount == 0);
+    REQUIRE(ledAlertCallCount == 0);
+     
+    const float maxThreshold = 110.2;
+    check_and_alert(maxThreshold, alerters, computedStats);
     REQUIRE(emailAlertCallCount == 1);
     REQUIRE(ledAlertCallCount == 1);
+    
+    // need a way to check if both emailAlerter, ledAlerter were called
+    // you can define call-counters along with the functions, as shown below
+    const float maxThreshold = 110.2;
+    check_and_alert(maxThreshold, alerters, computedStats);
+    REQUIRE((emailAlertCallCount == 1) && (ledAlertCallCount == 1));
+  
 }
