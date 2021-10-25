ccc96ba74b7b3b8d7d21a033bfb56904c9bc4d62
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2021-10-25 15:21:45 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -17,7 +17,6 @@ TEST_CASE("reports average, minimum and maximum") {
 }
 
 TEST_CASE("average is NaN for empty array") {
-     float numberset[] = {};
     struct Stats computedStats = compute_statistics(numberset, 0);
     //struct Stats computedStats = compute_statistics(0, 0);   
     //All fields of computedStats (average, max, min) must be
@@ -26,9 +25,9 @@ TEST_CASE("average is NaN for empty array") {
     //Design the REQUIRE statement here.
     //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
    
-     REQUIRE(isnan(computedStats.average));
-     REQUIRE(isnan(computedStats.max));
-     REQUIRE(isnan(computedStats.min));
+     REQUIRE(isnan(computedStats.average) == 0);
+     REQUIRE(isnan(computedStats.max) == 0);
+     REQUIRE(isnan(computedStats.min) == 0);
     
 }
     
