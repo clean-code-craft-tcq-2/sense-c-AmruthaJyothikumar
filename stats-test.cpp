e86d921ac910cd8645f6ed37bf3f01dbdb3c9155
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2021-10-25 10:10:33 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -14,10 +14,10 @@ TEST_CASE("reports average, minimum and maximum") {
     REQUIRE(abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(abs(computedStats.max - 8.9) < epsilon);
     REQUIRE(abs(computedStats.min - 1.5) < epsilon);
+}
 
-
-SECTION("average is NaN for empty array") {
-    Stats computedStats = compute_statistics(0, 0);   
+TEST_CASE("average is NaN for empty array") {
+    struct Stats computedStats = compute_statistics(0, 0);   
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
